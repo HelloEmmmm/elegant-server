77b67683a95ca049fd25c671720047df02e0a09b
Riches <riches.chiu@gmail.com>
2024-04-15 17:43:48 +0800

    chore: update code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { SuccessInterceptor } from './success/success.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new SuccessInterceptor());
+  app.use(helmet());
   await app.listen(3001);
 }
 
